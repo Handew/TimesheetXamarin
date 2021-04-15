229bbe2ceaf38c9dce17a957dbd618be6f3ee53a
Hannu Ilvonen <hannu.ilvonen@gmail.com>
2021-04-15 11:13:39 +0300

    eka commit

---
+++ XamarinTimesheet/EmployeePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinTimesheet
+{
+    public partial class EmployeePage : ContentPage
+    {
+        public EmployeePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
