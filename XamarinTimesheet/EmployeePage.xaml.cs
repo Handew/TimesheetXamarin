b68c60e1b0566454e69ce1139d0fd1c5658d0a84
Hannu Ilvonen <hannu.ilvonen@gmail.com>
2021-04-15 13:44:44 +0300

    testaus

--- XamarinTimesheet/EmployeePage.xaml.cs
+++ XamarinTimesheet/EmployeePage.xaml.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Newtonsoft.Json;
+using System.Net.Http;
+using XamarinTimesheet.Models;
+using System.Collections.ObjectModel;
 
 namespace XamarinTimesheet
 {
@@ -13,6 +17,44 @@ namespace XamarinTimesheet
         public EmployeePage()
         {
             InitializeComponent();
+
+            LoadEmployees();
+
+            async void LoadEmployees()
+            {
+                try
+                {
+                    employeeList.ItemsSource = new List<string> { "Ladataan", "IoT dataa", "palvelimelta..." };
+
+                    HttpClient client = new HttpClient();
+
+                    client.BaseAddress = new Uri("https://timesheetbackend.azurewebsites.net");
+                    string json = await client.GetStringAsync("/api/employees/");
+
+                    IEnumerable<Employee> employees = JsonConvert.DeserializeObject<Employee[]>(json);
+                    ObservableCollection<Employee> dataa = new ObservableCollection<Employee>(employees);
+                    employeeList.ItemsSource = dataa;
+
+                }
+                catch (Exception e)
+                {
+                    await DisplayAlert("Virhe: ", e.Message.ToString(), "SELVÄ!");
+                }
+            }
+        }
+
+        async void navbutton_Clicked(object sender, EventArgs e)
+        {
+            Employee emp = (Employee)employeeList.SelectedItem;
+            if (emp == null)
+            {
+                await DisplayAlert("Valinta puuttuu", "Valitse työntekijä", "OK"); // (otsiko, teksti, kuittausnapin teksti)
+            }
+            else
+            {
+                //await DisplayAlert("Valinta puuttuu", emp.FirstName, "OK");
+                await Navigation.PushAsync(new WorkAssignmentPage()); // Navigoidaan uudelle sivulle
+            }
         }
     }
 }
--- XamarinTimesheet/obj/Debug/netstandard2.0/EmployeePage.xaml.g.cs
+++ XamarinTimesheet/obj/Debug/netstandard2.0/EmployeePage.xaml.g.cs
@@ -17,8 +17,16 @@ namespace XamarinTimesheet {
     public partial class EmployeePage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.ListView employeeList;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button navbutton;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(EmployeePage));
+            employeeList = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "employeeList");
+            navbutton = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "navbutton");
         }
     }
 }
