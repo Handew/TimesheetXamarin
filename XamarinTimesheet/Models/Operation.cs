1b710cf03db356edb495db9739060a173292fabf
Hannu Ilvonen <hannu.ilvonen@gmail.com>
2021-04-29 14:00:14 +0300

    Projekti toimii, sijainti lisätty, työtehtävien hyväksyminen ja valmistuminen

---
+++ XamarinTimesheet/Models/Operation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinTimesheet.Models
+{
+    public class Operation
+    {
+        public int EmployeeID { get; set; }
+        public int? CustomerID { get; set; }
+        public int WorkAssidnmentID { get; set; }
+        public string OperationType { get; set; }
+
+        public string Comment { get; set; }
+
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+    }
+}
--- XamarinTimesheet/WorkAssignmentPage.xaml.cs
+++ XamarinTimesheet/WorkAssignmentPage.xaml.cs
@@ -9,21 +9,74 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using System.Collections.ObjectModel;
 using XamarinTimesheet.Models;
+using Xamarin.Essentials;
+using System.Threading;
 
 namespace XamarinTimesheet
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WorkAssignmentPage : ContentPage
     {
-        public WorkAssignmentPage(int id)
+        int eId;
+        string lon;
+        string lat;
+
+        public WorkAssignmentPage(int idParam)
         {
             InitializeComponent();
+
+            eId = idParam;
+
+            lon_label.Text = "Sijaintia haetaan";
 
             Työ_lataus.Text = "Ladataan työtehtäviä...";
 
             LoadDataFromRestAPI();
 
-            async void LoadDataFromRestAPI()
+            _ = GetCurrentLocation();
+
+            //--------------------lokaatio-----------------
+
+            async Task GetCurrentLocation()
+            {
+                try
+                {
+                    var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+                    var location = await Geolocation.GetLocationAsync(request);
+
+                    if (location != null)
+                    {
+                        lat_label.Text = ($"Longitude: { location.Longitude}");
+                        lon_label.Text = ($"Latitude: {location.Latitude}");
+
+                        lon = location.Longitude.ToString();
+                        lat = location.Latitude.ToString();
+                    }
+                }
+                catch (FeatureNotSupportedException fnsEx)
+                {
+                    // Handle not supported on device exception
+                    await DisplayAlert("Virhe", fnsEx.ToString(), "OK");
+                }
+                catch (FeatureNotEnabledException fneEx)
+                {
+                    // Handle not enabled on device exception
+                    await DisplayAlert("Virhe", fneEx.ToString(), "OK");
+                }
+                catch (PermissionException pEx)
+                {
+                    // Handle permission exception
+                    await DisplayAlert("Virhe", pEx.ToString(), "OK");
+                }
+                catch (Exception ex)
+                {
+                    // Unable to get location
+                    await DisplayAlert("Virhe", ex.ToString(), "OK");
+                }
+            }
+
+
+        async void LoadDataFromRestAPI()
             {
                 try
                 {
@@ -31,14 +84,14 @@ namespace XamarinTimesheet
 
                     HttpClient client = new HttpClient();
 
-                    client.BaseAddress = new Uri("https://timesheetbackend.azurewebsites.net");
+                    client.BaseAddress = new Uri("https://timesheetbackend.azurewebsites.net/");
                     string json = await client.GetStringAsync("/api/workassignments/");
 
                     IEnumerable<WorkAssignment> works = JsonConvert.DeserializeObject<WorkAssignment[]>(json);
                     ObservableCollection<WorkAssignment> dataa = new ObservableCollection<WorkAssignment>(works);
                     workList.ItemsSource = dataa;
                     //Tyhjennetään latausilmoitus
-                    Työ_lataus.Text = null;
+                    Työ_lataus.Text = "";
 
                 }
                 catch (Exception e)
@@ -48,14 +101,119 @@ namespace XamarinTimesheet
             }
         }
 
-        private void Aloitus_Nappi_Clicked(object sender, EventArgs e)
+        async void Aloitus_Nappi_Clicked(object sender, EventArgs e)
         {
+            WorkAssignment wa = (WorkAssignment)workList.SelectedItem;
 
+            if (wa == null)
+            {
+                await DisplayAlert("Valinta puuttuu", "Valitse työtehtävä", "OK");
+            }
+
+            try
+            {
+
+                Operation op = new Operation
+                {
+                    EmployeeID = eId,
+                    WorkAssidnmentID = wa.IdWorkAssingment,
+                    CustomerID = wa.IdCustomer,
+                    OperationType = "start",
+                    Comment = "Aloitettu",
+                    Latitude = lat,
+                    Longitude = lon
+                };
+
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("https://timesheetbackend.azurewebsites.net/");
+
+                // Muutetaan em. op objekti Jsoniksi
+                string input = JsonConvert.SerializeObject(op);
+                StringContent content = new StringContent(input, Encoding.UTF8, "application/json");
+
+                // Lähetetään serialisoitu objekti back-endiin Post pyyntönä
+                HttpResponseMessage message = await client.PostAsync("/api/workassignments", content);
+
+                // Otetaan vastaan palvelimen vastaus
+                string reply = await message.Content.ReadAsStringAsync();
+
+                // Asetetaan vastaus serialisoituna success muuttujaan
+                bool success = JsonConvert.DeserializeObject<bool>(reply);
+
+                if (success == false)
+                {
+                    await DisplayAlert("Ei voida aloittaa", "Työ on jo käynnissä", "OK");
+                }
+                else if (success == true)
+                {
+                    await DisplayAlert("Työ aloitettu", "Työ on aloitettu", "OK");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Virhe: ", ex.Message.ToString(), "SELVÄ");
+            }
         }
 
-        private void Lopetus_Nappi_Clicked(object sender, EventArgs e)
+        async void Lopetus_Nappi_Clicked(object sender, EventArgs e)
         {
 
+            WorkAssignment wa = (WorkAssignment)workList.SelectedItem;
+            
+
+            if (wa == null)
+            {
+                await DisplayAlert("Valinta puuttuu", "Valitse työtehtävä", "OK");
+                return;
+            }
+            string result = await DisplayPromptAsync("Kommentti", "Kirjoita kommentti");
+
+            try
+            {
+                Operation op = new Operation 
+                {   
+                    EmployeeID = eId, 
+                    WorkAssidnmentID = wa.IdWorkAssingment, 
+                    CustomerID = wa.IdCustomer, 
+                    OperationType = "stop",
+                    Comment = result,
+                    Latitude = lat,
+                    Longitude = lon
+                };
+
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("https://timesheetbackend.azurewebsites.net/");
+
+                // Muutetaan em. op objekti Jsoniksi
+                string input = JsonConvert.SerializeObject(op);
+                StringContent content = new StringContent(input, Encoding.UTF8, "application/json");
+
+                // Lähetetään serialisoitu objekti back-endiin Post pyyntönä
+                HttpResponseMessage message = await client.PostAsync("/api/workassignments", content);
+
+                // Otetaan vastaan palvelimen vastaus
+                string reply = await message.Content.ReadAsStringAsync();
+
+                // Asetetaan vastaus serialisoituna success muuttujaan
+                bool success = JsonConvert.DeserializeObject<bool>(reply);
+
+                if (success == false)
+                {
+                    await DisplayAlert("Ei voida lopettaa", "Työtä ole aloitettu", "OK");
+                }
+                else if (success == true)
+                {
+                    await DisplayAlert("Työn päättyminen", "Työ on päättynyt", "OK");
+
+                    await Navigation.PushAsync(new WorkAssignmentPage(eId));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Virhe: ", ex.Message.ToString(), "SELVÄ");
+            }
         }
     }
 }
